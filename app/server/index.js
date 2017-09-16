efbbdf557700ebfb54390aae77f0054a2088b231
Scott Serage <xttocsurge51@gmail.com>
2017-09-15 19:31:12 -0600

    Moved the build directory to inside of the public directory

--- app/server/index.js
+++ app/server/index.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const app = express();
 
 app.use(bodyParser.json());
-app.use(express.static(`${__dirname}/../build`));
+app.use(express.static(`${__dirname}/../public/build`));
 
 const port = 3000;
 app.listen(port, () => {console.log(`Server listening ${port.toLocaleString()} leagues under the sea...`)});
